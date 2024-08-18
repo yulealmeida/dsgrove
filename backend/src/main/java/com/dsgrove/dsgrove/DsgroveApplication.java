47097797c08b7593314c94152c7a0dcd5a2cf06c
Yule Almeida <yule.alme2@gmail.com>
2024-08-18 10:45:51 -0300

    Project created

---
+++ backend/src/main/java/com/dsgrove/dsgrove/DsgroveApplication.java
@@ -0,0 +1,13 @@
+package com.dsgrove.dsgrove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsgroveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsgroveApplication.class, args);
+	}
+
+}
